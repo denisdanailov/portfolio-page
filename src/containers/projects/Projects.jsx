93cb27f6fc47a06acdb46b75d8b8edeceb4d595c
Denis Danailov <denis.danailov@gmx.de>
2022-09-04 12:13:35 +0200

    edit Projects section

--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -19,7 +19,6 @@ const Projects = () => (
                 You can see the rest of my Projects on my GitHub profile.</p>
             
             <Section name="App-cap" image={appcap} url="https://app-cap.netlify.app/" />
-<<<<<<< HEAD
             <Section name="SecondParts" image={todo} url="https://github.com/denisdanailov/secondparts" />
             <Section name="Weather App" image={weather} url="https://github.com/denisdanailov/weather-app" />
            
